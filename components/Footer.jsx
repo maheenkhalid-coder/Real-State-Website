a0e7c0a49920c32cd7bd010e8eb40a00fde0e986
maheenkhalid-coder <64140831+maheenkhalid-coder@users.noreply.github.com>
2022-03-05 22:28:33 +0500

    second commit

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/layout';
+
+const Footer = () => (
+  <Box textAlign='center' p='5' color='Black' borderTop='1px' borderColor='gray.100'>
+    © 2022 RealState, Inc.
+  </Box>
+);
+
+export default Footer;
